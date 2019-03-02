119bfbe25739cae56e4883ab3fe0f9199d86c7c5
Chavaughn <chavaughwilks@yahoo.com>
2019-03-02 11:27:56 -0500

    I'm dying on on the inside

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/FtcRobotControllerActivity.java
@@ -102,9 +102,7 @@ import org.firstinspires.ftc.ftccommon.internal.FtcRobotControllerWatchdogServic
 import org.firstinspires.ftc.ftccommon.internal.ProgramAndManageActivity;
 import org.firstinspires.ftc.robotcore.external.navigation.MotionDetection;
 import org.firstinspires.ftc.robotcore.internal.hardware.DragonboardLynxDragonboardIsPresentPin;
-import org.firstinspires.ftc.robotcore.internal.network.DeviceNameManager;
 import org.firstinspires.ftc.robotcore.internal.network.DeviceNameManagerFactory;
-import org.firstinspires.ftc.robotcore.internal.network.WifiDirectDeviceNameManager;
 import org.firstinspires.ftc.robotcore.internal.network.PreferenceRemoterRC;
 import org.firstinspires.ftc.robotcore.internal.network.StartResult;
 import org.firstinspires.ftc.robotcore.internal.network.WifiMuteEvent;
@@ -648,19 +646,19 @@ public class FtcRobotControllerActivity extends Activity
       cfgFileMgr.setActiveConfigAndUpdateUI(false, file);
     }
 
-    OpModeRegister userOpModeRegister = createOpModeRegister();
-    eventLoop = new FtcEventLoop(hardwareFactory, userOpModeRegister, callback, this, programmingModeController);
-    FtcEventLoopIdle idleLoop = new FtcEventLoopIdle(hardwareFactory, userOpModeRegister, callback, this, programmingModeController);
+//    OpModeRegister userOpModeRegister = createOpModeRegister();
+//    eventLoop = new FtcEventLoop(hardwareFactory, userOpModeRegister, callback, this, programmingModeController);
+//    FtcEventLoopIdle idleLoop = new FtcEventLoopIdle(hardwareFactory, userOpModeRegister, callback, this, programmingModeController);
 
     controllerService.setCallback(callback);
-    controllerService.setupRobot(eventLoop, idleLoop, runOnComplete);
+//    controllerService.setupRobot(eventLoop, idleLoop, runOnComplete);
 
     passReceivedUsbAttachmentsToEventLoop();
   }
 
-  protected OpModeRegister createOpModeRegister() {
-    return new FtcOpModeRegister();
-  }
+//  protected OpModeRegister createOpModeRegister() {
+////    return new FtcOpModeRegister();
+//  }
 
   private void shutdownRobot() {
     if (controllerService != null) controllerService.shutdownRobot();
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp6899/Autonomous6899.java
@@ -0,0 +1,153 @@
+package org.firstinspires.ftc.teamcode.TeleOp6899;
+
+import com.disnodeteam.dogecv.CameraViewDisplay;
+import com.disnodeteam.dogecv.DogeCV;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous
+public class Autonomous6899 extends LinearOpMode {
+
+    Hardware6899 Bot;
+
+    private enum CompetitionLegs{
+        Delatch,Vuforia, Mineral, Marker, Crater, Finish}
+        CompetitionLegs Legs;
+
+    private GoldAlignDetector detector;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private double getruntime;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        detector = new GoldAlignDetector(); // Create detector
+        detector.init(hardwareMap.appContext, CameraViewDisplay.getInstance()); // Initialize it with the app context and camera
+        detector.useDefaults(); // Set detector to use default settings
+        //tuning
+        detector.alignSize = 100; // How wide (in pixels) is the range in which the gold object will be aligned. (Represented by green bars in the preview)
+        detector.alignPosOffset = 0; // How far from center frame to offset this alignment zone.
+        detector.downscale = 0.4; // How much to downscale the input frames
+
+        detector.areaScoringMethod = DogeCV.AreaScoringMethod.MAX_AREA; // Can also be PERFECT_AREA
+        //detector.perfectAreaScorer.perfectArea = 10000; // if using PERFECT_AREA scoring
+        detector.maxAreaScorer.weight = 0.005; //
+
+        detector.ratioScorer.weight = 5; //
+        detector.ratioScorer.perfectRatio = 1.0; // Ratio adjustment
+        Bot.init(hardwareMap);
+
+        detector.enable();
+        waitForStart();
+
+
+        while (opModeIsActive()) {
+
+            runtime.reset();
+            if (detector.isFound()){
+                telemetry.addLine("Found Cube!");
+                telemetry.update();
+                continue;
+            }
+            if (!detector.isFound() && runtime.seconds() <= 3 && opModeIsActive()) {
+                telemetry.addLine("Looking for Cube");
+                telemetry.update();
+                //Looking for Cube
+                Rotate(1, .2, 1000);
+                if (detector.isFound()) {
+                    telemetry.addLine("Found Cube!");
+                    telemetry.update();
+                    getruntime = runtime.seconds();
+                }
+
+                //Opposite direction
+                telemetry.addLine("Looking for Cube");
+                telemetry.update();
+                Rotate(1, -.2, 1000);
+                if (detector.isFound()) {
+                    telemetry.addLine("Found Cube!");
+                    telemetry.update();
+                    getruntime = runtime.seconds();
+                }
+            }
+
+            telemetry.addLine("Grabbing the Cube!");
+            telemetry.update();
+            //Goes to cube and grabs it
+            RunToPosDrive(4, 0.5, 3000);
+            Bot.IntakeL.setPosition(1);
+            Bot.IntakeR.setPosition(0);
+
+            telemetry.addLine("Moving back to Original Pos.");
+            telemetry.update();
+            //Moves back to original position
+            RunToPosDrive(4, -0.5, 3000);
+            Bot.IntakeL.setPosition(0.5);
+            Bot.IntakeR.setPosition(0.5);
+
+            //Rotates back into original position
+            runtime.reset();
+
+                Rotate(1, 0.2, 1000);
+
+            //Just for testing - stops Opmode
+        }
+    }
+
+    private void Rotate(int inches,double power, int sleeptimer){
+        Bot.FrontLeft.setTargetPosition(inches);
+        Bot.FrontRight.setTargetPosition(inches);
+        Bot.BackRight.setTargetPosition(inches);
+        Bot.BackLeft.setTargetPosition(inches);
+        Bot.FrontRight.setPower(-power);
+        Bot.BackLeft.setPower(power);
+        Bot.FrontLeft.setPower(power);
+        Bot.BackRight.setPower(-power);
+        sleep(sleeptimer);
+        ZeroDrive();
+        RunwithEncAll();
+    }
+
+    private void RunToPos(DcMotor motor, int inches, double power){
+        motor.setPower(power);
+        motor.setTargetPosition(inches);
+    }
+
+    private void RunToPosDrive(int inches, double power, int sleeptimer){
+        Bot.FrontLeft.setTargetPosition(inches);
+        Bot.FrontRight.setTargetPosition(inches);
+        Bot.BackRight.setTargetPosition(inches);
+        Bot.BackLeft.setTargetPosition(inches);
+        Bot.FrontRight.setPower(-power);
+        Bot.BackLeft.setPower(-power);
+        Bot.FrontLeft.setPower(-power);
+        Bot.BackRight.setPower(-power);
+        sleep(sleeptimer);
+        ZeroDrive();
+        RunwithEncAll();
+    }
+
+    private void RunwithEncAll(){
+        Bot.FrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Bot.FrontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Bot.BackLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Bot.BackRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    private void Zero(DcMotor motor){
+        motor.setPower(0);
+    }
+
+    private void ZeroDrive(){
+        Bot.FrontLeft.setPower(0);
+        Bot.FrontRight.setPower(0);
+        Bot.BackRight.setPower(0);
+        Bot.BackLeft.setPower(0);
+    }
+}
+
